7bde260430aa6fe2f14da2631c384539c73f11fb
sefy <sefy1600@gmail.com>
2017-08-12 17:25:27 +0300

    Login/register fragments

---
+++ app/src/main/java/com/sefy/finalproject/Auth/AuthActivity.java
@@ -0,0 +1,39 @@
+package com.sefy.finalproject.Auth;
+import android.app.Activity;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.sefy.finalproject.R;
+
+public class AuthActivity extends Activity implements RegisterFragment.OnFragmentInteractionListener, LoginFragment.OnFragmentInteractionListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+
+        LoginFragment loginFragment = new LoginFragment();
+        /**
+         * setting transaction in order to display register fragment
+         */
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.add(R.id.auth_frag_container,loginFragment);
+        transaction.commit();
+    }
+
+    @Override
+    public void onFragmentInteraction() {
+
+    }
+
+    @Override
+    public void onSubmitLogin(String userEmail, String userPassword) {
+        Log.d("TAG","Auth activity recieved: "+ userEmail+" "+userPassword);
+    }
+
+    @Override
+    public void onClickRegisterButton() {
+        Log.d("TAG","display register fragment");
+    }
+}
---
+++ app/src/main/java/com/sefy/finalproject/Auth/LoginFragment.java
@@ -0,0 +1,89 @@
+package com.sefy.finalproject.Auth;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.sefy.finalproject.R;
+
+public class LoginFragment extends Fragment {
+
+    private OnFragmentInteractionListener mListener;
+    private EditText email,password;
+    private Button login,register;
+
+    public LoginFragment() {
+    }
+
+    public static LoginFragment newInstance() {
+        LoginFragment fragment = new LoginFragment();
+
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View contentView= inflater.inflate(R.layout.fragment_login, container, false);
+        this.email = (EditText) contentView.findViewById(R.id.login_fragment_email);
+        this.password = (EditText) contentView.findViewById(R.id.login_fragment_password);
+
+        this.login = (Button) contentView.findViewById(R.id.login_fragment_submit);
+        this.register = (Button) contentView.findViewById(R.id.login_fragment_register_page);
+
+        /**
+         * setting listeners
+         */
+        this.login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userEmail = email.getText().toString();
+                String userPassword = password.getText().toString();
+                mListener.onSubmitLogin(userEmail,userPassword);
+            }
+        });
+        this.register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mListener.onClickRegisterButton();
+            }
+        });
+        return contentView;
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+        void onSubmitLogin(String userEmail, String userPassword);
+        void onClickRegisterButton();
+    }
+}
