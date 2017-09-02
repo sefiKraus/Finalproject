04b937767ded91ee2fee42213474aae048ebc0fe
sefy <sefy1600@gmail.com>
2017-09-02 15:48:03 +0300

    Done item_details_fragment

--- app/src/main/java/com/sefy/finalproject/Item/ItemDetailsFragment.java
+++ app/src/main/java/com/sefy/finalproject/Item/ItemDetailsFragment.java
@@ -1,32 +1,43 @@
 package com.sefy.finalproject.Item;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.sefy.finalproject.Cart.CartListService;
+import com.sefy.finalproject.Model.CartItem;
+import com.sefy.finalproject.Model.ImageManager;
 import com.sefy.finalproject.Model.ItemModel;
 import com.sefy.finalproject.R;
 
 public class ItemDetailsFragment extends Fragment {
 
-    private static final String ITEM_NAME = "param1";
-    private static final String ITEM_DESCRIPTION = "param2";
-    private static final String ITEM_PRICE = "";
-    private static final String BRAND_NAME = "param2";
+    private static final String ITEM_NAME = "itemName";
+    private static final String ITEM_DESCRIPTION = "itemDescription";
+    private static final String ITEM_PRICE = "itemPrice";
+    private static final String BRAND_NAME = "brandName";
+
+    private TextView iName , iDescription , iPrice;
+    private ImageView iImage;
+    private ImageButton addToCart;
 
 
     private int itemPrice;
     private String brandName;
     private String itemName;
     private String itemDescription;
-
+    static ItemModel currentItem = null;
 
     public ItemDetailsFragment() {
-        // Required empty public constructor
     }
 
 
@@ -37,7 +48,7 @@ public class ItemDetailsFragment extends Fragment {
         args.putString(ITEM_DESCRIPTION, itemModel.getDescription());
         args.putString(ITEM_PRICE, String.valueOf(itemModel.getPrice()));
         args.putString(BRAND_NAME, itemModel.getBrandName());
-
+        currentItem = itemModel;
         fragment.setArguments(args);
         return fragment;
     }
@@ -57,7 +68,37 @@ public class ItemDetailsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View contentView = inflater.inflate(R.layout.fragment_item_details, container, false);
+        this.iName = (TextView) contentView.findViewById(R.id.item_details_fragment_itemName);
+        this.iDescription = (TextView) contentView.findViewById(R.id.item_details_fragment_description);
+        this.iImage = (ImageView) contentView.findViewById(R.id.item_details_fragment_image);
+        this.iPrice = (TextView) contentView.findViewById(R.id.item_details_fragment_price);
 
+        this.iName.setText(currentItem.getName());
+        this.iPrice.setText(String.valueOf(currentItem.getPrice()));
+        this.iDescription.setText(currentItem.getDescription());
+
+        this.addToCart = (ImageButton) contentView.findViewById(R.id.item_details_fragment_addToCart);
+        this.addToCart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CartListService.getInstance().addToCart(new CartItem(currentItem,1 , currentItem.getPrice()));
+                Toast.makeText(getActivity(),"Item added to shopping list",Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+        ImageManager imageman= new ImageManager();
+        imageman.loadImageFromCache(currentItem.getImage(), new ImageManager.GetImageListener() {
+            @Override
+            public void onSuccess(Bitmap image) {
+                iImage.setImageBitmap(image);
+            }
+
+            @Override
+            public void onFail() {
+
+            }
+        });
         return contentView;
     }
 
